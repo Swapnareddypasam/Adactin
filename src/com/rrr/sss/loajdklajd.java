e26fba24f06ca33986779080e11a6ed71bb97f41
venka <venka@RamiReddy>
2024-02-03 10:34:27 +0530

    Sikuli

---
+++ src/com/rrr/sss/loajdklajd.java
@@ -0,0 +1,21 @@
+package com.rrr.sss;
+
+
+
+	import org.sikuli.script.Screen;
+	public class loajdklajd {
+	public static void main(String args[]) throws Exception{
+	    //Test Steps
+	Screen sc = new Screen();
+	Thread.sleep(3000);
+	sc.click("D:\\Software Testing Naresh IT\\Snap\\s1.PNG\\");
+	Thread.sleep(3000);
+	sc.doubleClick("D:\\Software Testing Naresh IT\\Snap\\VKD.PNG\\");
+	Thread.sleep(3000);
+	sc.click("D:\\Software Testing Naresh IT\\Snap\\VKC.PNG\\");
+	Thread.sleep(3000);
+	sc.doubleClick("D:\\Software Testing Naresh IT\\Snap\\PCD.PNG\\");
+	Thread.sleep(3000);
+	sc.click("D:\\Software Testing Naresh IT\\Snap\\PCC.PNG\\");
+	}
+	}
